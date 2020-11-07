8f84e70d178de4f9a0cff57d11c1b630cc4222c2
Rafael Ferreira <rafaelferreira.dev3@gmail.com>
2020-11-07 13:31:20 -0300

    produzindo novas funcoes

--- testesJunit/src/test/java/testes/CalculadoraTeste.java
+++ testesJunit/src/test/java/testes/CalculadoraTeste.java
@@ -17,4 +17,48 @@ public class CalculadoraTeste {
 
         assertEquals(3, soma);
     }
+
+    @Test
+    public void subtraiDoisValores(){
+        int valorA = 1;
+        int valorB = 1;
+
+        Calculadora calculadora = new Calculadora();
+        int subtracao = calculadora.subtracao(valorA, valorB);
+
+        assertEquals(0,subtracao);
+    }
+
+    @Test
+    public void dividirDoisValores(){
+        int valorA = 2;
+        int valorB = 2;
+
+        Calculadora calculadora = new Calculadora();
+        int divisao = calculadora.divisao(valorA, valorB);
+
+        assertEquals(1, divisao);
+    }
+
+    @Test
+    public void multiplicarDoisValores(){
+        int valorA = 2;
+        int valorB = 2;
+
+        Calculadora calculadora = new Calculadora();
+        int multiplicacao = calculadora.multiplicacao(valorA, valorB);
+
+        assertEquals(4, multiplicacao);
+    }
+
+    @Test
+    public void potenciarDoisValores(){
+        int valorA = 2;
+        int valorB = 2  ;
+
+        Calculadora calculadora = new Calculadora();
+        int potencializacao = calculadora.potencia(valorA, valorB);
+
+        assertEquals(4, potencializacao);
+    }
 }
